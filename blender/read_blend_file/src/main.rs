345f0741a6842b84ac5f8812c8eb813534af15ed
Jan Walter <jan@janwalter.com>
2014-10-13 21:51:51 +0200

    Read 7 bytes from file (should be "BLENDER") and close file again.

--- blender/read_blend_file/src/main.rs
+++ blender/read_blend_file/src/main.rs
@@ -1,4 +1,4 @@
-// use std::io::File;
+use std::io::File;
 use std::os;
 use std::fmt;
 
@@ -18,7 +18,32 @@ fn main() {
     if args.len() != 2 {
         println!("usage: read_blend_file /path/to/file.blend");
     } else {
-        println!("open \"{}\" ...", args[1]);
+        let slice: &str = args[1].as_slice();
+        println!("open \"{}\" ...", slice);
+        {
+            // read 7 bytes from the Blender file
+            let path = Path::new(slice);
+            let display = path.display();
+            let mut file = match File::open(&path) {
+                Err(why) => fail!("couldn't open {}: {}", display, why.desc),
+                Ok(file) => file,
+            };
+            let mut buf = [0u8, ..7];
+            match file.read(buf) {
+                Err(why) => fail!("couldn't open {}: {}", display, why.desc),
+                Ok(_) => (),
+            }
+            println!("bytes read: {}{}{}{}{}{}{}", 
+                     buf[0] as char,
+                     buf[1] as char,
+                     buf[2] as char,
+                     buf[3] as char,
+                     buf[4] as char,
+                     buf[5] as char,
+                     buf[6] as char);
+            // TODO: assert 7 bytes read are "BLENDER"
+        } // file gets closed here
+        // code below is temporary
         let z = Complex { re: -1.0, im: 0.0 };
         let z_sqrt = unsafe {
             csqrtf(z)
