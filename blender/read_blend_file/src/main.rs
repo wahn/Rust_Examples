25883329a03b09e57f1edb56479e807ade6f26ab
Jan Walter <jan@janwalter.com>
2014-10-13 20:48:15 +0200

    This is temporary code to show how to use a Foreign Function Interface (FFI) to C libraries.

--- blender/read_blend_file/src/main.rs
+++ blender/read_blend_file/src/main.rs
@@ -1,5 +1,17 @@
 // use std::io::File;
 use std::os;
+use std::fmt;
+
+#[repr(C)]
+struct Complex {
+    re: f32,
+    im: f32,
+}
+
+#[link(name = "m")]
+extern {
+    fn csqrtf(z: Complex) -> Complex;
+}
 
 fn main() {
     let args = os::args();
@@ -7,5 +19,20 @@ fn main() {
         println!("usage: read_blend_file /path/to/file.blend");
     } else {
         println!("open \"{}\" ...", args[1]);
+        let z = Complex { re: -1.0, im: 0.0 };
+        let z_sqrt = unsafe {
+            csqrtf(z)
+        };
+        println!("the square root of {} is {}", z, z_sqrt);
+    }
+}
+
+impl fmt::Show for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}i", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}i", self.re, self.im)
+        }
     }
 }
