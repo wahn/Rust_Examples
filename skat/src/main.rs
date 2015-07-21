c10c0fc6660be8003d99cf417cab50d37a4b5027
Jan Walter <jdb.walter@gmail.com>
2015-07-21 06:59:13 +0200

    Define 32 cards and 3 players (as enums).

--- skat/src/main.rs
+++ skat/src/main.rs
@@ -1,3 +1,44 @@
+enum Card {
+    ClubsAce,
+    ClubsTen,
+    ClubsKing,
+    ClubsQueen,
+    ClubsJack,
+    ClubsNine,
+    ClubsEight,
+    ClubsSeven,
+    SpadesAce,
+    SpadesTen,
+    SpadesKing,
+    SpadesQueen,
+    SpadesJack,
+    SpadesNine,
+    SpadesEight,
+    SpadesSeven,
+    HeartsAce,
+    HeartsTen,
+    HeartsKing,
+    HeartsQueen,
+    HeartsJack,
+    HeartsNine,
+    HeartsEight,
+    HeartsSeven,
+    DiamondsAce,
+    DiamondsTen,
+    DiamondsKing,
+    DiamondsQueen,
+    DiamondsJack,
+    DiamondsNine,
+    DiamondsEight,
+    DiamondsSeven,
+}
+
+enum Player {
+    A,
+    B,
+    C,
+}
+
 fn main() {
     println!("Hello, world!");
 }
