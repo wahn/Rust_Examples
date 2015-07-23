64e0d8d782728a790775295ae0562aab45dd986d
Jan Walter <jdb.walter@gmail.com>
2015-07-23 22:51:14 +0200

    Skeleton for playing a 10 trick game. Ask for another round.

--- skat/src/main.rs
+++ skat/src/main.rs
@@ -38,15 +38,36 @@ enum Card {
     DiamondsSeven,
 }
 
+enum Game {
+    Suit,
+    Grand,
+    Null,
+}
+
 enum Player {
     A,
     B,
     C,
 }
 
+fn bid(dealer: u8) -> (u8, u8) {
+    let winner: u8 = dealer;
+    let highest: u8 = 18;
+    // TODO
+    (winner, highest)
+}
+
+fn deal(dealer: u8) {
+    // TODO
+}
+
+fn announce(player: u8) -> Game {
+    Game::Suit
+}
+
 fn main() {
     // randomly select player
-    let player_number = rand::thread_rng().gen_range(0, 3);
+    let player_number: u8 = rand::thread_rng().gen_range(0, 3);
     match player_number {
         0 => println!("player A:"),
         1 => println!("player B:"),
@@ -54,6 +75,27 @@ fn main() {
         _ => panic!("Uknown player {}", player_number),
     }
     loop {
+        // player player_number is dealing
+        deal(player_number);
+        // bid
+        let (player, bid) = bid(player_number);
+        // announce
+        let game = announce(player);
+        // play 10 tricks in a row
+        for trick in 0..10 {
+            println!("trick #{}:", trick);
+            // ask for input
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)
+                .ok()
+                .expect("failed to read line");
+            let input: u8 = match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => break,
+            };
+            // is input valid?
+        };
+        println!("New game?");
         let mut input = String::new();
         io::stdin().read_line(&mut input)
             .ok()
@@ -63,5 +105,8 @@ fn main() {
             Err(_) => break,
         };
         println!("your input: {}", input);
+        if input != 1 {
+            break;
+        }
     }
 }
